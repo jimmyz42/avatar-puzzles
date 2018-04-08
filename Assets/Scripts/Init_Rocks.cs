9d5243b9149cf49aa69aa4abe5c9cfe12620ad33
Jimmy <3.1415926.candlexyz.pi+github@gmail.com>
2018-04-08 00:29:26 -0400

    made rocks interactable

---
+++ Assets/Scripts/Init_Rocks.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init_Rocks : MonoBehaviour {
+
+	public float sceneStartTimer;
+	public GameObject rockTemplate;
+	public int rows = 10;
+	public int cols = 10;
+	public int numRocks = 20;
+	private int rock_w = 30, rock_d = 30;
+	private Dictionary<Vector2Int, GameObject> rocks = new Dictionary<Vector2Int, GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(InitRockTimer());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (checkWinCondition ()) {
+			onGameWin ();
+		}
+	}
+
+	IEnumerator InitRockTimer()
+	{
+		yield return new WaitForSeconds(sceneStartTimer);
+		initRocks ();
+	}
+
+	private void initRocks() {
+		HashSet<Vector2Int> points = new HashSet<Vector2Int> ();
+		while(points.Count < numRocks) {
+			points.Add (new Vector2Int (Random.Range (0, rows), Random.Range (0, cols)));
+		}
+		foreach (Vector2Int point in points) {
+			Vector3 pos = getTransformPos (point);
+			GameObject rock = Instantiate(rockTemplate, pos, Quaternion.identity);
+			InteractableRockController control = rock.GetComponent<InteractableRockController> ();
+			control.setManager (this);
+			control.setPosition (point);
+			rocks.Add (point, rock);
+		}
+	}
+
+	public Vector2Int moveRock(Vector2Int pos, int dx, int dy) {
+		Vector2Int cur = pos;
+		while(inBoundsNotRock(new Vector2Int(cur.x+dx, cur.y+dy))) {
+			cur = new Vector2Int(cur.x+dx, cur.y+dy);
+		}
+		GameObject rock = rocks [pos];
+		rocks.Remove (pos);
+		rocks.Add (cur, rock);
+		return cur;
+	}
+
+	private bool inBoundsNotRock(Vector2Int p) {
+		return p.x >= 0 && p.y >= 0 && p.x < cols && p.y < rows && !rocks.ContainsKey(p);
+	}
+
+	public Vector3 getTransformPos(Vector2Int p, float y = 22) {
+		return new Vector3 ((p.x - (cols - 1) / 2.0f) * rock_w, y, (p.y - (rows - 1) / 2.0f) * rock_d);
+	}
+
+	// Check for win condition
+	public bool checkWinCondition () {
+		int middleCol = cols / 2;
+		for (int i = 0; i < rows; i++) {
+			if (!rocks.ContainsKey (new Vector2Int (middleCol, i)))
+				return false;
+		}
+		return true;
+	}
+
+	// Use this handler to do whatever actions need to be taken when game is won
+	public void onGameWin() {
+		// TODO Brianna
+	}
+}
--- Assets/Scripts/InteractableRockController.cs
+++ Assets/Scripts/InteractableRockController.cs
@@ -3,33 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractableRockController : MonoBehaviour {
+	// NOTE to abilities team: Please call the following methods:
+	// selectRock(), unselectRock(), slideRock(dir)
+
+	// Assume that game grid is [-150, 150] x [-150, 150]
+	// rocks are 30 x 30, so 10 x 10 grid
 
     public float timer;
+	public float speed;
+	public enum Direction {RIGHT, UP, LEFT, DOWN};
+	public static GameObject selectedRock = null;
+	private Init_Rocks manager;
+	private Vector2Int position;
+	private Vector3 targetPos;
 
-    private float x_cord;
-    private float z_cord;
+	private static int[] delta_x = { 1, 0, -1, 0 };
+	private static int[] delta_z = { 0, 1, 0, -1 };
+
 	// Use this for initialization
 	void Start ()
     {
-        x_cord = transform.position.x;
-        z_cord = transform.position.z;
-        StartCoroutine(AddingMovableRocks());	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.anyKey) {
+			if (Input.GetKey (KeyCode.UpArrow))
+				slideRock (Direction.UP);
+			if (Input.GetKey (KeyCode.DownArrow))
+				slideRock (Direction.DOWN);
+			if (Input.GetKey (KeyCode.LeftArrow))
+				slideRock (Direction.LEFT);
+			if (Input.GetKey (KeyCode.RightArrow))
+				slideRock (Direction.RIGHT);
+		}
+		// Move the rock
+		Vector3 target = targetPos;
+		target.y = gameObject.transform.position.y; // don't change y, only x and z
+		gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target, speed * Time.deltaTime);
+	}
+
+	void OnMouseDown() {
+		if (selectedRock != gameObject) {
+			unselectRock (); // unselect that rock
+			selectRock (); // select this rock
+		} else {
+			unselectRock (); // clicking selected rock unselects it
+		}
+	}
+
+	public void setManager(Init_Rocks manager) {
+		this.manager = manager;
+	}
+
+	public void setPosition(Vector2Int p) {
+		this.position = p;
+		this.targetPos = manager.getTransformPos (p, 25);
 	}
 
     public void Appear()
     {
         gameObject.SetActive(true);
     }
-    IEnumerator AddingMovableRocks()
-    {
-        yield return new WaitForSeconds(timer);
-        this.transform.position = new Vector3(x_cord, 22, z_cord);
 
+	public void selectRock() {
+		selectedRock = gameObject;
+		Vector3 temp = selectedRock.transform.position;
+		temp.y = 25;
+		selectedRock.transform.position = temp;
+	}
 
-    }
+	public void unselectRock() {
+		if (selectedRock != null) {
+			Vector3 temp = selectedRock.transform.position;
+			temp.y = 22;
+			selectedRock.transform.position = temp;
+		}
+		selectedRock = null;
+	}
+
+	public void slideRock(Direction direction) {
+		if (selectedRock != gameObject)
+			return; // invalid, can only move self
+		// selectedRock is this.gameObject, so just move self
+		int dir = (int)direction;
+		setPosition(manager.moveRock (this.position, delta_x [dir], delta_z [dir]));
+	}
 }
