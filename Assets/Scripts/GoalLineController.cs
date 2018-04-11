a951b5cecf698114d766385f5b32702cb81219f8
toshiro1200 <brigbi@mit.edu>
2018-04-11 00:37:38 -0400

    Added Path for rocks

---
+++ Assets/Scripts/GoalLineController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalLineController : MonoBehaviour {
+
+    public bool canMove;
+    private Vector3 endpos;
+    public float delay;
+    public float speed;
+	// Use this for initialization
+	void Start ()
+    {
+        canMove = false;
+        endpos = new Vector3(0, transform.position.y, 0);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (canMove)
+        //{
+            StartCoroutine(MovingGoalLine());
+        //}
+        
+		
+	}
+    
+    void AppearingOnTheBoard()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, endpos, speed* Time.deltaTime);
+    }
+
+    IEnumerator MovingGoalLine()
+    {
+        yield return new WaitForSeconds(delay);
+        AppearingOnTheBoard();
+    }
+}
