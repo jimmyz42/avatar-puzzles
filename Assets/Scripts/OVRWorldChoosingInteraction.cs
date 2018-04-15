d283073fe713d6b1363e9e741b30056b492a3208
toshiro1200 <brigbi@mit.edu>
2018-04-14 21:35:43 -0400

    HH

---
+++ Assets/Scripts/OVRWorldChoosingInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRWorldChoosingInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSelect( Transform t)
+    {
+        if (t.gameObject.tag == "EarthWorld")
+        {
+            Debug.Log("Selected the earth world");
+            RoomWorldController earthWorldController = t.gameObject.GetComponent<RoomWorldController>();
+            earthWorldController.isSelected = true;
+            //earthWorldController.transitioning = true;
+            //StartCoroutine(earthWorldController.EnteringTheWorld());
+        }
+        else
+        {
+            Debug.Log("Selected something else");
+        }
+
+    }
+}
