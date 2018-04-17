b937893608b548da897939c665c7f6cff83f260c
toshiro1200 <brigbi@mit.edu>
2018-04-17 13:41:36 -0400

    Mirror done

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Mirror : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    public Transform MirrorCam;
+    public Transform PlayerCam;
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        CalculateRotation();	
 	}
+
+    public void CalculateRotation()
+    {
+        Vector3 dir = (PlayerCam.position - transform.position).normalized;
+        Quaternion rot = Quaternion.LookRotation(dir);
+
+        rot.eulerAngles = transform.eulerAngles - rot.eulerAngles;
+
+        MirrorCam.localRotation = rot;
+    }
+    private void OnBecameInvisible()
+    {
+        Debug.Log("not here");
+    }
+
 }
