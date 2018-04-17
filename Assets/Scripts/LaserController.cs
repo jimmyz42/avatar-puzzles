f439c6126c5b965c86846030794496b04740fa78
toshiro1200 <brigbi@mit.edu>
2018-04-17 18:21:59 -0400

    Done with Laser

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -4,13 +4,68 @@ using UnityEngine;
 
 public class LaserController : MonoBehaviour {
 
+    public LineRenderer line;
+    public Transform startPos;
+    //public Vector3 ori;
+    //public Vector3 moved;
+    public bool laserOn;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+        //startPos = gameObject.GetComponentInParent<Transform>();
+        line.enabled = false;
+        //ori = transform.position;
+        //transform.position = startPos.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (laserOn)
+        {
+            StartCoroutine(MakeLaser());
+        }
+        
 	}
+
+    IEnumerator MakeLaser()
+    {
+        while (laserOn)
+        {
+            line.enabled = true;
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            //moved = ray.origin;
+            line.SetPosition(0, ray.origin);
+            line.useWorldSpace = true;
+
+           if (Physics.Raycast(ray, out hit, 1000))
+            {
+               /* if (hit.collider != null)
+                {
+                Debug.Log("hit something");
+                Debug.Log(hit.transform.name);
+                Vector3 newPoint = hit.point - transform.forward;
+                line.SetPosition(1, newPoint);
+
+                }*/
+                //Debug.Log(hit.transform.name);
+                //Debug.DrawLine(ray.origin, hit.point,Color.cyan);
+            }
+            //else
+            //{*/
+            line.SetPosition(1, ray.GetPoint(10000));
+            //}
+            
+
+            yield return null;
+        }
+
+    }
+
+    public void TurnLaserOn(bool yes)
+    {
+        laserOn = yes;
+    }
 }
--- Assets/Scripts/LevelConroller.cs
+++ Assets/Scripts/LevelConroller.cs
@@ -6,14 +6,17 @@ public class LevelConroller : MonoBehaviour {
 
     public GameObject Walls;
     public GameObject Leaves;
+    public GameObject LaserStart;
     public float wallSpeed;
     public float wallDelay;
     public float leavesDrops;
     public float leavesFallDelay;
+    public float gameStartDelay;
     public SettingCenter[] Twirl;
 
     private WallController walls;
     private LeavesController leaves;
+    private LaserController laser;
     
 
 	// Use this for initialization
@@ -22,6 +25,8 @@ public class LevelConroller : MonoBehaviour {
         walls = Walls.GetComponent<WallController>();
         Walls.SetActive(false);
         leaves = Leaves.GetComponent<LeavesController>();
+        laser = LaserStart.GetComponent<LaserController>();
+
         //leaves.delay = leavesDrops;
 	}
 	
@@ -35,7 +40,7 @@ public class LevelConroller : MonoBehaviour {
     {
         yield return new WaitForSeconds(wallDelay);
         Walls.SetActive(true);
-        walls.SetParams(true, wallSpeed);
+        walls.SetParams(true, wallSpeed, gameStartDelay);
         yield return new WaitForSeconds(leavesFallDelay);
         leaves.SetParams(true, leavesDrops);
 
