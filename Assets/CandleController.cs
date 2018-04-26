2a306268db6c48206289ae96132962b9048262f8
toshiro1200 <brigbi@mit.edu>
2018-04-26 17:28:50 -0400

    Making Fire Scene

---
+++ Assets/CandleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleController : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject fire;
+    public bool TurnFireOn;
+
+    void Start ()
+    {
+        fire = gameObject.transform.GetChild(0).gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (TurnFireOn)
+        {
+            fire.SetActive(true);
+        }
+        else
+        {
+            fire.SetActive(false);
+        }
+	}
+
+    public void PutFireOn(bool t)
+    {
+        TurnFireOn = t;
+    }
+}
