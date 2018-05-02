9290bc82ff3bc527750c0fc5d1024fb45028c88f
toshiro1200 <brigbi@mit.edu>
2018-05-02 18:41:13 -0400

    Goals for Water

---
+++ Assets/Scripts/GoalHoleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GoalHoleScript : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="WaterPipe")
+        {
+            EventManager.TriggerEvent("TurnOnALakeWaterfall");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "WaterPipe")
+        {
+            EventManager.TriggerEvent("TurnOffALakeWaterfall");
+        }
+    }
+}
