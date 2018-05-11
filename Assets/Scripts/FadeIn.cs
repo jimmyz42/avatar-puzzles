1299f90ec9aa25198a1c723a493fa61736e2b116
toshiro1200 <brigbi@mit.edu>
2018-05-11 17:59:28 -0400

    Intro

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour {
+
+    public Texture2D fadeOutTexture;
+    public float fadeSpeed = .8f;
+
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+    private int fadeDir = -1;
+
+    private void OnGUI()
+    {
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.depth = drawDepth;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+
+    }
+
+    public float BeginFade(int direction)
+    {
+        fadeDir = direction;
+        return (fadeSpeed);
+    }
+
+    public void FadeOut()
+    {
+        alpha = 1.0f;
+        BeginFade(-1);
+    }
+    private void OnLevelWasLoaded()
+    {
+        alpha = 1.0f;
+        BeginFade(-1);
+    }
+}
---
+++ Assets/TransistionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransistionScript : MonoBehaviour {
+
+    public float videoEndtime = 115.0f;
+    public FadeIn fade;
+	// Use this for initialization
+	void Start ()
+    {
+        fade = gameObject.GetComponent<FadeIn>();
+        StartCoroutine(IntroChange());
+	}
+	
+    IEnumerator IntroChange()
+    {
+        
+        yield return new WaitForSeconds(videoEndtime);
+        float fadetime = fade.BeginFade(1);
+        yield return new WaitForSeconds(fadetime);
+        SceneManager.LoadScene("AstralRoom", LoadSceneMode.Additive);
+    }
+}
