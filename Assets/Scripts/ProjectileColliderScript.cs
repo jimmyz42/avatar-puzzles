003058bcdfe14b50cda4eb738bb0443372330d0b
toshiro1200 <brigbi@mit.edu>
2018-05-13 01:12:25 -0400

    fg

--- Assets/Scripts/ProjectileColliderScript.cs
+++ Assets/Scripts/ProjectileColliderScript.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ProjectileColliderScript : MonoBehaviour {
+
+
+    [System.Serializable]
+    public class SelectionCallback : UnityEvent { }
+
+    public ProjectileColliderScript.SelectionCallback onMirrorCollision;
+
 
     // Use this for initialization
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.tag != "Player")
+        string HitTag = collision.collider.tag;
+        if( HitTag != "Player" || HitTag =="GameMirror")
         {
             gameObject.SetActive(false);
-        }
+            onMirrorCollision.Invoke();
+        }   
     }
 }
