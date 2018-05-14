af8311ca41c973ff9085d62d53dc5de6c81f5db3
toshiro1200 <brigbi@mit.edu>
2018-05-13 22:01:04 -0400

    Preview Scripts

---
+++ Assets/Scripts/ClosePreviewScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClosePreviewScript : MonoBehaviour {
+
+    private UnityAction close;
+    // Use this for initialization
+    private void Awake()
+    {
+        close = new UnityAction(ExitPreview);
+    }
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("ClosePreview", close);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("ClosePreview", close);
+    }
+
+    void ExitPreview()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PreviewScene.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PreviewScene : MonoBehaviour {
+
+    public bool PreviewShown;
+    public float previewTime;
+    public int sceneNum;
+	// Use this for initialization
+	void Start ()
+    {
+        PreviewShown = false;
+        switch (this.name)
+        {
+            case "EarthWorld":
+                sceneNum = 1;
+                break;
+            case "AirWorld":
+                sceneNum = 2;
+                break;
+            case "FireWorld":
+                sceneNum = 3;
+                break;
+            case "WaterWorld":
+                sceneNum = 4;
+                break;
+            default:
+                sceneNum = 0;
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 1;
+            EventManager.TriggerEvent("ClosePreview");
+        }
+	}
+
+    private void OnMouseEnter()
+    {
+        
+        if (!PreviewShown && sceneNum > 0)
+        {
+            Time.timeScale = 0;
+            SceneManager.LoadScene(sceneNum, LoadSceneMode.Additive);
+            PreviewShown = true;
+        }
+        
+        //Application.LoadLevelAdditive(1);
+    }
+    private void OnMouseExit()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(DestroyPreview());
+    }
+
+    IEnumerator DestroyPreview()
+    {
+        yield return new WaitForSeconds(previewTime);
+        EventManager.TriggerEvent("ClosePreview");
+    }
+}
