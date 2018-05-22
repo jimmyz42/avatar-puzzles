b11a1b217a18c78cbd69905a6a9e274bb6e4df65
toshiro1200 <brigbi@mit.edu>
2018-05-22 14:24:16 -0400

    User-Friendly

---
+++ Assets/GettingAudioForVideo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GettingAudioForVideo : MonoBehaviour {
+
+    public VideoPlayer vd;
+    public AudioSource aud;
+    // Use this for initialization
+    private void Awake()
+    {
+        vd = gameObject.GetComponent<VideoPlayer>();
+        aud = GameObject.Find("OSTAudio").GetComponent<AudioSource>();
+
+        Debug.Log(vd.audioOutputMode);
+        Debug.Log(vd.GetTargetAudioSource(0));
+        //vd.SetTargetAudioSource()
+        vd.SetTargetAudioSource(0, aud);
+        Debug.Log(vd.GetTargetAudioSource(0));
+        
+    }
+
+
+}
